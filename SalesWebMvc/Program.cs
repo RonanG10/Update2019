1dc38a3d5a744bf4a2812104b52b01d46e7c25e5
Ronan Gomes <ronangjunior.rg@gmail.com>
2026-05-21 14:51:17 -0300

    MySQL adaptation and first migration

--- SalesWebMvc/Program.cs
+++ SalesWebMvc/Program.cs
@@ -1,9 +1,20 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using SalesWebMvc.Data;
+using MySqlConnector;
+using Microsoft.Extensions.Configuration;
+using System.Configuration;
+
 var builder = WebApplication.CreateBuilder(args);
+
 builder.Services.AddDbContext<SalesWebMvcContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("SalesWebMvcContext") ?? throw new InvalidOperationException("Connection string 'SalesWebMvcContext' not found.")));
+    options.UseMySql(
+        builder.Configuration.GetConnectionString("SalesWebMvcContext"),
+        ServerVersion.AutoDetect(
+            builder.Configuration.GetConnectionString("SalesWebMvcContext")
+        )
+    ));
+
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
