a4a8d5e514850421625ab2770badd1216a97de7b
Ronan Gomes <ronangjunior.rg@gmail.com>
2026-05-21 21:56:53 -0300

    Seeding Service

--- SalesWebMvc/Program.cs
+++ SalesWebMvc/Program.cs
@@ -1,45 +1,56 @@
 using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.DependencyInjection;
 using SalesWebMvc.Data;
-using MySqlConnector;
-using Microsoft.Extensions.Configuration;
-using System.Configuration;
+
 
 var builder = WebApplication.CreateBuilder(args);
 
+// Connection String
+var connectionString = builder.Configuration.GetConnectionString("SalesWebMvcContext");
+
+// DbContext
 builder.Services.AddDbContext<SalesWebMvcContext>(options =>
     options.UseMySql(
-        builder.Configuration.GetConnectionString("SalesWebMvcContext"),
-        ServerVersion.AutoDetect(
-            builder.Configuration.GetConnectionString("SalesWebMvcContext")
-        )
+        connectionString,
+        ServerVersion.AutoDetect(connectionString)
     ));
 
+// Services
+builder.Services.AddScoped<SeedingService>();
 
-// Add services to the container.
+// MVC
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+// Pipeline
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
 app.UseHttpsRedirection();
+
 app.UseRouting();
 
 app.UseAuthorization();
 
-app.MapStaticAssets();
+app.UseStaticFiles();
 
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}")
     .WithStaticAssets();
 
+
+// Executa o Seed
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+
+    var seedingService = services.GetRequiredService<SeedingService>();
+
+    seedingService.Seed();
+}
 
 app.Run();
