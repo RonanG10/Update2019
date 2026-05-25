dab5790d73ad0c8b3ec41b4969047cdfd2c425cb
Ronan Gomes <ronangjunior.rg@gmail.com>
2026-05-25 15:02:46 -0300

    Foreign key not null (referential integrity)

--- SalesWebMvc/Controllers/SellersController.cs
+++ SalesWebMvc/Controllers/SellersController.cs
@@ -29,7 +29,7 @@ namespace SalesWebMvc.Controllers
         public IActionResult Create(Seller seller)
         {
             _sellerService.Insert(seller);
-            return RedirectToAction("Index");
+            return RedirectToAction(nameof(Index));
         }
     }
 }
